0456ad2bd83f43261b3c654f66a9781259c2f901
kirushikan <kirushikankiru01@gmail.com>
2025-10-06 14:10:14 +0530

    food display commponents

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import StoreContextProvider from './context/StroreContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-  <App />
+    <StoreContextProvider>
+      <App />
+    </StoreContextProvider>
+  
+  
   </BrowserRouter>
     
   
