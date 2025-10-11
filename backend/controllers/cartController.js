3d2781f85f70588586a9ae00de652d55c5176374
kirushikan <kirushikankiru01@gmail.com>
2025-10-11 10:46:21 +0530

    auth.js for the cart details

--- backend/controllers/cartController.js
+++ backend/controllers/cartController.js
@@ -2,7 +2,13 @@ import userModel from "../models/userModel.js";
 
 //add item to user cart
 const addToCart = async (req,res) =>{
+    try{
+        let userData = await userModel.findOne({_id:req.body.userId});
+        let cartData = await userData.cartData;
+        
+    } catch (error){
 
+    }
 }
 
 //remove item from user cart
